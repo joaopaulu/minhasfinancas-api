a01deb6c7f8bb321df9bc6b42948ee2b0d695183
João Paulo Lima <jptick@gmail.com>
2020-12-31 14:34:53 -0300

    Implement Valid Email Method and Create Services

---
+++ src/main/java/io/github/joaopaulu/minhasfinancas/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package io.github.joaopaulu.minhasfinancas.exception;
+
+public class RegraNegocioException extends RuntimeException{
+    public RegraNegocioException(String msg){
+        super(msg);
+    }
+}
--- src/main/java/io/github/joaopaulu/minhasfinancas/repository/UsuarioRepository.java
+++ src/main/java/io/github/joaopaulu/minhasfinancas/repository/UsuarioRepository.java
@@ -4,4 +4,6 @@ import io.github.joaopaulu.minhasfinancas.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
 }
